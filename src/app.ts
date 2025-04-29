d6ec43934a1f055fa9d22ed8970c2c614e496326
Rob Nascimento <zuranmrs2@gmail.com>
2025-04-29 14:42:51 -0300

    chore: integrate Prisma, update dependencies, and modify app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify  from 'fastify';
+import { PrismaClient } from './generated/prisma';
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Roberta Nascimento',
+    }
+})
